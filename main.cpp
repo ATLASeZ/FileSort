2e779487500eb395b0c73d0e74b012352c73a6a2
Ruslan <113227667+ATLASeZ@users.noreply.github.com>
2024-04-26 15:06:52 +0000

    started develop lab

--- main.cpp
+++ main.cpp
@@ -1,5 +1,58 @@
 #include <iostream>
+#include <fstream>
+#include <istream>
+#include <random>
 
-int main() {
-  std::cout << "Hello World!\n";
+using std::cout;
+using std::endl;
+using std::string;
+using std::ofstream;
+using std::ifstream;
+
+// Функция проверки открытия файла для записи
+bool IsFileOpen(ofstream& file) {
+    if (!file.is_open()) {
+        cout << "Ошибка открытия файла!" << endl;
+
+        return false;
+    }
+    else {
+        cout << "Файл успешно открыт!" << endl;
+
+        return true;
+    }
+}
+
+// Функция проверки открытия файла для чтения
+bool IsFileOpen(ifstream& file) {
+    if (!file.is_open()) {
+        cout << "Ошибка открытия файла!" << endl;
+
+        return false;
+    }
+    else {
+        cout << "Файл успешно открыт!" << endl;
+
+        return true;
+    }
+}
+
+// Функция создания файла со случайным массивом чисел
+bool CreateFileWithRandomNumbers(const string& fileName, const int numbersCount, const int maxNumerValue) {
+    ofstream file;
+    file.open(fileName);
+
+    IsFileOpen(file);
+
+    std::random_device randomDevice;
+    std::mt19937 generation(randomDevice());
+    std::uniform_int_distribution<> distribution(0, maxNumerValue);
+
+    for (int i = 0; i < numbersCount; ++i) {
+        file << distribution(generation) << " ";
+    }
+
+    file.close();
+
+    return true;
 }
